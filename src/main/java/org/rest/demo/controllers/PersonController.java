c4a7fc84263d00864418081bee60072bfee7a4e0
Bushy-Given <bushy.netshidaulu@gmail.com>
2018-05-09 13:59:33 +0200

    Added Swagger2 API Documentation

--- src/main/java/org/rest/demo/controllers/PersonController.java
+++ src/main/java/org/rest/demo/controllers/PersonController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("api")
 public class PersonController {
 
     private PersonService personService;
